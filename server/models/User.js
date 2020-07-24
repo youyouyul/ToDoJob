860c3c958b3d1859950724d1702246dee3ac757c
yuri <yulyuleee@gmail.com>
2020-07-24 17:51:03 +0900

    react setting

---
+++ server/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 10
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 8
+    }
+})

3477d115df033777ca4fd24b59deb828d93c5841
yuri <yulyuleee@gmail.com>
2020-07-30 12:46:33 +0900

    dashboard header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import '../../assets/css/header.css';
+import '../../assets/css/fonts.css';
+import logo from '../../assets/image/logo.png';
+import profile from '../../assets/image/profile.png';
 
 function Header() {
-    const style = {
-        /* 레이아웃 */
-        display: 'flex',
-        position: 'fixed',
-        height: '60px',
-        width: '100%',
-        top: '0px',
-        zIndex: '5',
-
-        backgroundColor: 'white',
-
-        /* 폰트 */
-        fontSize: '2.5rem'
-    }
-
-    const headerStyle = {
-        position: 'fixed',
-        width: '100%',
-        zIndex: '5',
-    }
 
     return (
-        <nav className='header' style={ headerStyle }>
-            <div className="header_logo">
-                <a href='/'><img style={{ width: '40%', alignItems: 'center' }} src={ logo } alt="logo"/></a>
-                <span>
+        <nav className='bg-white flex p-2 items-center' role='navigation'>
+            <div className='header md:flex flex-grow'>
+                { /* logo */ }
+                <a className='header_logo' href='/dashboard'><img src={ logo } alt="logo"/></a>
+                <div className='fontStyle pt-1' style={{ padding: '5px 10px', fontSize: '1.2rem'}}>
                     TO DO JOB
-                </span>
+                </div> 
+            </div>
+
+            { /* search & profile */ }
+            <div className='md:flex ml-4 items-center justify-end' style={{ padding: '0 10px'}}>
+                <div className='items-center ml-3 pt-1' style={{ padding: '0 10px'}}>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type="text" placeholder="할 일 검색" />
+                </div>
+                <a className='header_logo' href='/dashboard'><img src={ profile } alt="profile"/></a>
             </div>
         </nav>
-
     )
 }
 
---
+++ client/src/components/views/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../layout/Header'
+
+function Dashboard() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Dashboard

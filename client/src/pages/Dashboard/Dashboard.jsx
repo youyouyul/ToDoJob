0754fa0d2939d9eca46507a6f66b0b38f0a46afa
yuri <yulyuleee@gmail.com>
2020-08-05 17:44:48 +0900

    TodoList template

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import Header from '../../components/layout/Header/Header';
-import '../Dashboard/dashboard.css';
+import Footer from '../../components/layout/Footer/Footer';
+import ToDoList from '../../components/views/ToDoList/ToDoList';
+import dashboartStyle from '../Dashboard/dashboard.module.css';
 
 function Dashboard() {
     return (
         <div>
             <Header />
-            {/* To Do */}
-            <div className='boxContainer'>
-                * TO-DO LIST
-                <div className="boxToDo">
-                    boxToDo
-                    <br />
-                    dob
-                </div>
+            <div className={ dashboartStyle.boxContainer } >
+                {/* To Do */}
+                <ToDoList />
+
+
+                {/* 바로가기 */}
+
+                {/* 서류 */}
+                
+                {/* 시험 */}
+                
+                {/* 1차 면접*/}
+                
+                {/* 2차 면접 */}
             </div>
-
-
-            {/* 바로가기 */}
-
-            {/* 서류 */}
             
-            {/* 시험 */}
-            
-            {/* 1차 면접*/}
-            
-            {/* 2차 면접 */}
-
+            <Footer />
         </div>
     )
 }
